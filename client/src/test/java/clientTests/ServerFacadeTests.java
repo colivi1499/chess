f6466dfb253fcb66406d044fc35628798ba303e3
Cameron Olivier <cameronolivier@Camerons-Air-3.app.byu.edu>
2024-03-19 13:29:05 -0600

    Wrote negative tests for register and login

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -37,11 +37,24 @@ public class ServerFacadeTests {
     }
 
     @Test
+    public void registerBadUsername() throws DataAccessException {
+        facade.register("SomeoneElse2","Password123", "johndoe@gmail.com");
+        Assertions.assertThrows(DataAccessException.class, () -> facade.register("SomeoneElse2", "some_password", "an_email"));
+    }
+
+    @Test
     public void login() throws DataAccessException {
         var authData = facade.register("SomeoneElse2","Password123", "johndoe@gmail.com");
         var authData2 = facade.login("SomeoneElse2","Password123");
         Assertions.assertTrue(authData.authToken().length() > 10 && authData2.authToken().length() > 10);
         Assertions.assertTrue(!authData.authToken().equals(authData2.authToken()));
+    }
+
+    @Test
+    public void badLogin() throws DataAccessException {
+        facade.register("SomeoneElse2","Password123", "johndoe@gmail.com");
+        Assertions.assertThrows(DataAccessException.class, () -> facade.login("SomeoneElse2", "a wrong password"));
+        Assertions.assertThrows(DataAccessException.class, () -> facade.login("Not a user", "Password12345"));
     }
 
 }
