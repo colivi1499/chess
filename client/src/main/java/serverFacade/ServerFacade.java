4514917f2426471a0c7ed2ee8f94b064e23357b6
Cameron Olivier <cameronolivier@Camerons-Air-3.app.byu.edu>
2024-03-19 13:08:58 -0600

    Added ServerFacade positive register test

---
+++ client/src/main/java/serverFacade/ServerFacade.java
@@ -0,0 +1,85 @@
+package serverFacade;
+
+import com.google.gson.Gson;
+import dataAccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+import server.Server;
+
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.*;
+
+public class ServerFacade {
+
+    private final int port;
+    private final String serverUrl;
+
+    public ServerFacade(int port) {
+        this.port = port;
+        this.serverUrl = "http://localhost:" + port;
+    }
+
+    public AuthData register(String username, String password, String email) throws DataAccessException {
+        var path = "/user";
+        return this.makeRequest("POST", path, new UserData(username, password, email), AuthData.class);
+    }
+
+    public void clear() throws DataAccessException {
+        var path = "/db";
+        this.makeRequest("DELETE", path, null, null);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws DataAccessException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request,http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, DataAccessException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new DataAccessException("failure: " + status);
+        }
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,18 +1,27 @@
 package clientTests;
 
+import dataAccess.DataAccessException;
 import org.junit.jupiter.api.*;
 import server.Server;
+import serverFacade.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
+    }
+
+    @BeforeEach
+    public void setup() throws DataAccessException {
+        facade.clear();
     }
 
     @AfterAll
@@ -22,8 +31,9 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void register() throws DataAccessException {
+        var authData = facade.register("SomeoneElse2","Password123", "johndoe@gmail.com");
+        Assertions.assertTrue(authData.authToken().length() > 10);
     }
 
 }
