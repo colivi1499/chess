b358dd1614b54aff28363cf7e44214778309c941
Cameron Olivier <cameronolivier@Camerons-MacBook-Air-3.local>
2024-04-10 15:47:17 -0600

    Added UserGameCommand subclasses

---
+++ shared/src/main/java/webSocketMessages/userCommands/JoinPlayer.java
@@ -0,0 +1,13 @@
+package webSocketMessages.userCommands;
+
+import chess.ChessGame;
+
+public class JoinPlayer extends UserGameCommand {
+    int gameID;
+    ChessGame.TeamColor teamColor;
+    public JoinPlayer(String authToken, int gameID, ChessGame.TeamColor color) {
+        super(authToken);
+        this.gameID = gameID;
+        this.teamColor = color;
+    }
+}
